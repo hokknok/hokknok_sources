fe0c5d62b6b3c7b49d43526ff7142399ee8b2332
hokknok <jeepcreep@mail.ru>
2025-08-05 09:11:44 +0300

    + Pascal_Triangle.py

--- Pascal_Triangle.py
+++ Pascal_Triangle.py
@@ -1,15 +1,18 @@
 def generate(numRows):
-        rows = [[] for _ in range(numRows)]
+        rows = [[] for _ in range(1)]
         rows[0].append(1)
-        i = 1
+        print(rows)
+        i = 1 #second row with 2 elements
         while i < numRows:
-            row = [None] * i
+            row = [None] * (i + 1)
 
-            row[0], row[i - 1] = 1, 1
-            for j in range(1, i - 1):
+            row[0], row[-1] = 1, 1
+            for j in range(1, i):
                 row[j] = rows[i - 1][j - 1] + rows[i - 1][j]
 
-            rows[i].append(row)
+            rows.append(row)
+            print(rows)
+            i += 1
 
         print(rows)
 
