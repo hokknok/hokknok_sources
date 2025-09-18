e2840fbf9f41833febf4b6d2ddea3d58ea734eaf
hokknok <jeepcreep@mail.ru>
2025-09-18 21:50:21 +0300

    + 1668. Maximum Repeating Substring

---
+++ 1668. Maximum Repeating Substring.py
@@ -0,0 +1,16 @@
+
+class Solution:
+    def maxRepeating(self, sequence: str, word: str) -> int:
+        for i in range()
+
+def main():
+
+    repeating = Solution()
+    sequence = "aaaba aaaba aab aaaa b aaaa b aaaa b aaaa b a"
+    sequence = "aaaba aaaba aab a aaaba aaaba aaaba aaaba"
+    word = "aaaba"
+
+    print(repeating.maxRepeating(sequence,word))
+
+if __name__ == "__main__":
+    main()
