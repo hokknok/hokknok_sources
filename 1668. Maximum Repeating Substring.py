95eafa0c9299fcfb4a3dc2fbab42b4ca11cabbf3
gromstorm <jeepcreep@mail.ru>
2025-09-18 23:35:06 +0300

    problem solved, but not optimal

--- 1668. Maximum Repeating Substring.py
+++ 1668. Maximum Repeating Substring.py
@@ -1,16 +1,32 @@
-
 class Solution:
     def maxRepeating(self, sequence: str, word: str) -> int:
-        for i in range()
+        count = [0] * len(sequence)
+        recword = word * (len(sequence) // len(word)) + word[:len(sequence) % len(word)]
+
+        print(recword)
+
+        for i in range(len(recword)):
+            for j in range(i, len(sequence)):
+                print("i =", i, "j =", j, "recword[i] =", recword[i], "sequence[j]", sequence[j])
+                if recword[i] == sequence[j] and count[j - i] == i:
+                    count[j - i] += 1
+                elif count[j - i] < len(word):
+                    count[j - i] = 0
+
+            print(count)
+
+        return max(count) // len(word)
+
 
 def main():
-
     repeating = Solution()
-    sequence = "aaaba aaaba aab aaaa b aaaa b aaaa b aaaa b a"
-    sequence = "aaaba aaaba aab a aaaba aaaba aaaba aaaba"
-    word = "aaaba"
+    # sequence = "aaabaaaabaaabaaaabaaaabaaaabaaaaba"
+    # word = "aaaba"
+    sequence = "ababc"
+    word = "ab"
 
-    print(repeating.maxRepeating(sequence,word))
+    print(repeating.maxRepeating(sequence, word))
+
 
 if __name__ == "__main__":
     main()
