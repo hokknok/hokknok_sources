42cce29e5e842d49a55f269634db960caaa41ed7
hokknok <jeepcreep@mail.ru>
2026-02-02 21:01:10 +0300

    2025,topic2,update problem4

--- ya_algs_lrng_2025_3_4_test4.py
+++ ya_algs_lrng_2025_3_4_test4.py
@@ -14,19 +14,20 @@ def main():
     while i < len(txt):
         k = bisect_left(dct, txt[i])
         while k < len(dct) and dct[k] == txt[i:i+len(dct[k])]:
+            el = 0
             for j in range(len(sqnsc)):
                 if sqnsc[j][-1] == i+len(dct[k]):
                     el = 0
                     break
-                elif sqnsc[j][-1] == i:
-                    el = i+len(dct[k])
-            else sqnsc[j].append(i+len(dct[k]))
+                elif sqnsc[j][-1] == i - 1:
+                    el = j + i + len(dct[k]) - 1
+            else:
+                sqnsc[el - i + len(dct[k]) - 1].append(i + len(dct[k]) - 1)
+                if i == len(txt) - 1 and sqnsc[el - i + len(dct[k]) - 1][-1] == i:
+                    print(sqnsc[el - i + len(dct[k]) - 1])
         #########################################
             k += 1
-        #nw_txt += txt[i:i+len(dct[k-1])] + ' '
-        i += len(dct[k-1])
-
-    print(nw_txt)
+        i += 1
 
 if __name__ == '__main__':
     main()
