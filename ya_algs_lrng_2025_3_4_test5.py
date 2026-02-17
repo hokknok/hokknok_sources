68d1d5b667428a52b42e0611cc1038cbaeef324b
hokknok <jeepcreep@mail.ru>
2026-02-17 20:39:35 +0300

    2025,topic2: problem4 update problem5 update

--- ya_algs_lrng_2025_3_4_test5.py
+++ ya_algs_lrng_2025_3_4_test5.py
@@ -5,23 +5,15 @@ dp = [0] * num
 minT = twrs[0]
 df = 0
 
-for i in range(K):
-    df += twrs[i]
-    minT = min(minT, twrs[i])
-dp[K - 1] = df * minT
-
-for i in range(K, num):
+for i in range(K - 1, num):
     df = 0
     minT = twrs[i]
-    for j in range(i, i - K, -1):
-        df += twrs[j]
-        minT = min(minT, twrs[j])
+    for j in range(K):
+        df += twrs[i - j]
+        minT = min(minT, twrs[i - j])
     df = df * minT
     df += dp[i - K]
     dp[i] = max(df, dp[i - 1] if K > 1 else df)
-
-#print(dp)
-
 count = 0
 answ = []
 df = dp[i]
@@ -29,14 +21,13 @@ while i - K + 1 >= 0:
     if i == 0:
         count += 1
         answ.append(i + 1)
-        break
     if dp[i - 1] < df:
         count += 1
         answ.append(i - K + 2)
-        i = i-K
+        i = i - K
         df = dp[i]
-        continue
-    i -= 1
+    else:
+        i -= 1
 
 print(count)
 print(*answ[::-1])
